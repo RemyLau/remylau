e338554140b667f47ac8db96d5a9bc5a9b56bbce
Renming Liu <liurenmi@msu.edu>
2023-01-03 15:47:03 -0500

    add default lsp servers

--- dotfiles/.config/nvim/after/plugin/lsp.lua
+++ dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -26,7 +26,12 @@ end
 require("mason-lspconfig").setup {
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
     -- This setting has no relation with the `automatic_installation` setting.
-    ensure_installed = {},
+    ensure_installed = {
+        "bashls",
+        "pylsp",
+        "sumneko_lua",
+        "yamlls",
+    },
 
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
     -- This setting has no relation with the `ensure_installed` setting.

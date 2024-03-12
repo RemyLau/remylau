0fd255fe122b0403204bb3de79578c41e588c7c0
Remy <liurenmi@msu.edu>
2024-03-12 10:49:04 -0400

    remove help module from tresitter, deprecated?

--- dotfiles/.config/nvim/after/plugin/treesitter.lua
+++ dotfiles/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "lua", "vim", "help" },
+  -- ensure_installed = { "python", "lua", "vim", "help" },
+  ensure_installed = { "python", "lua", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

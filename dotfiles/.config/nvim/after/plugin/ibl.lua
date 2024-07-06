2f31ae09c1810a21392cfd50aa247a3b219fd09e
Renming Liu <liurenmi@msu.edu>
2024-07-06 15:18:35 -0400

    upload new plugins configs

---
+++ dotfiles/.config/nvim/after/plugin/ibl.lua
@@ -0,0 +1,35 @@
+local highlight = {
+    "Function",
+    "Label",
+    -- "CursorColumn",
+    -- "Whitespace",
+    -- "IblIndent",
+    -- "IblScope",
+    -- "LineNr",
+}
+local char = "▏"
+-- local char = "▎"
+-- local char = "│"
+-- local char = "╎"
+-- local char = "┆"
+-- local char = "┊"
+
+require("ibl").setup {
+    enabled = true,
+    debounce = 200,
+    indent = {
+        -- highlight = highlight,
+        char = char,
+        smart_indent_cap = true,
+    },
+    viewport_buffer = { min = 30 },
+    scope = {
+        enabled = true,
+        char = char,
+        show_start = true,
+        show_end = true,
+        show_exact_scope = false,
+        injected_languages = true,
+        -- highlight = highlight,
+    },
+}

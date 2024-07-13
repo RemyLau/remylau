f69e76b9ac530a904dff2f94435525ee6f97922b
RemyLau <remylau961@gmail.com>
2024-07-13 10:07:20 -0400

    update tokyonight config

--- dotfiles/.config/nvim/after/plugin/color.lua
+++ dotfiles/.config/nvim/after/plugin/color.lua
@@ -1,8 +1,51 @@
 require('tokyonight').setup({
-    style = 'moon',
+    style = 'moon', -- moon, storm, night, day
     transparent = true,
     transparent_sidebar = true,
     dim_inactive = true,
+    lualine_bold = true,
+    styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+    },
+    -- Boarderless Telescope window
+    -- on_highlights = function(hl, c)
+    --     local prompt = "#2d3149"
+    --     hl.TelescopeNormal = {
+    --       bg = c.bg_dark,
+    --       fg = c.fg_dark,
+    --     }
+    --     hl.TelescopeBorder = {
+    --       bg = c.bg_dark,
+    --       fg = c.bg_dark,
+    --     }
+    --     hl.TelescopePromptNormal = {
+    --       bg = prompt,
+    --     }
+    --     hl.TelescopePromptBorder = {
+    --       bg = prompt,
+    --       fg = prompt,
+    --     }
+    --     hl.TelescopePromptTitle = {
+    --       bg = prompt,
+    --       fg = prompt,
+    --     }
+    --     hl.TelescopePreviewTitle = {
+    --       bg = c.bg_dark,
+    --       fg = c.bg_dark,
+    --     }
+    --     hl.TelescopeResultsTitle = {
+    --       bg = c.bg_dark,
+    --       fg = c.bg_dark,
+    --     }
+    -- end,
 })
 
 require('rose-pine').setup({

aade4b8df26c4beb50b093db5b7f5b6e097c85eb
RemyLau <remylau961@gmail.com>
2024-07-22 09:44:13 -0400

    formatting

--- dotfiles/.config/nvim/after/plugin/lualine.lua
+++ dotfiles/.config/nvim/after/plugin/lualine.lua
@@ -18,51 +18,51 @@ Prequire('lualine').setup({
         }
     },
     sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {
-    -- Copilot lualine settings
-    {
-        'copilot',
-        symbols = {
-            status = {
-            icons = {
-                enabled = " ",
-                sleep = " ",   -- auto-trigger disabled
-                disabled = " ",
-                warning = " ",
-                unknown = " "
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {
+            -- Copilot lualine settings
+            {
+                'copilot',
+                symbols = {
+                    status = {
+                        icons = {
+                            enabled = " ",
+                            sleep = " ",   -- auto-trigger disabled
+                            disabled = " ",
+                            warning = " ",
+                            unknown = " "
+                        },
+                        hl = {
+                            enabled = "#50FA7B",
+                            sleep = "#AEB7D0",
+                            disabled = "#6272A4",
+                            warning = "#FFB86C",
+                            unknown = "#FF5555"
+                        },
+                    },
+                    spinners = require("copilot-lualine.spinners").dots,
+                    spinner_color = "#6272A4"
+                },
+                show_colors = false,
+                show_loading = true
             },
-            hl = {
-                enabled = "#50FA7B",
-                sleep = "#AEB7D0",
-                disabled = "#6272A4",
-                warning = "#FFB86C",
-                unknown = "#FF5555"
-            },
-            },
-            spinners = require("copilot-lualine.spinners").dots,
-            spinner_color = "#6272A4"
+            ---
+            'encoding',
+            'fileformat',
+            'filetype',
         },
-        show_colors = false,
-        show_loading = true
-    },
-    ---
-    'encoding',
-    'fileformat',
-    'filetype',
-    },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
     },
     inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
     },
     tabline = {},
     winbar = {},
